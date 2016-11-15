afbe5286777a8775a118c6b36097bbec5529896c
Ed Copony <ecopony@execonline.com>
2016-11-15 10:15:25 -0800

    Fix return format

--- honeybadger/src/handler.js
+++ honeybadger/src/handler.js
@@ -2,5 +2,13 @@ import HoneybadgerSchedule from './HoneybadgerSchedule';
 
 module.exports.hello = (event, context, callback) => {
   const schedule = new HoneybadgerSchedule();
-  callback(null, schedule.message());
+  var response = {
+    statusCode: 200,
+    headers: {
+      "Content-Type" : "application/json"
+    },
+    body: schedule.message()
+  };
+  console.info("Response", JSON.stringify(response));
+  callback(null, response);
 };
